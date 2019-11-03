435dee240d373454246aa4c35d2a52c209937986
Zakepanka <arzybek@users.noreply.github.com>
2019-11-04 02:48:47 +0500

    Add Spiral Rectangles formation Change proj type to console Added tests Added simple visualiser

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -1,14 +1,36 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace TagsCloudVisualization
 {
     public class CircularCloudLayouter : MustInitialize<Point>, ICircularCloudLayouter
     {
         private Point _center;
-        
+        public List<Rectangle> field = new List<Rectangle>();
+
+        private double spiralCoeff = 1 / (2 * 3.14);
+        private double angleStep = 3.14 / 8;
+        private double angle = 0;
+
         public Rectangle PutNextRectangle(Size rectangleSize)
         {
-            return Rectangle.Empty;
+            var i = 0;
+            while (true)
+            {
+                i++;
+                var point = getNextSpiralPoint();
+                var top = _center.Y + point.Y + rectangleSize.Height / 2;
+                var left = _center.X + point.X - rectangleSize.Width / 2;
+                var rect = new Rectangle(new Point(left, top),
+                    rectangleSize); // point - левый верхний угол, size - ширина/высота
+                
+                var flag = CheckIntersection(rect);
+                if (flag)
+                    continue;
+                field.Add(rect);
+                return rect;
+            }
         }
 
         public CircularCloudLayouter(Point center) : base(center)
@@ -16,5 +38,24 @@ namespace TagsCloudVisualization
             this._center = center;
         }
         
+        public bool CheckIntersection(Rectangle getRect)
+        {
+            foreach (var rectangle in field)
+            {
+                if(getRect.IntersectsWith(rectangle))
+                    return true;
+            }
+
+            return false;
+        }
+
+        private Point getNextSpiralPoint()
+        {
+            int X = (int) Math.Floor(spiralCoeff * angle * Math.Cos(angle));
+            int Y = (int) Math.Floor(spiralCoeff * angle * Math.Sin(angle));
+            var point = new Point(X, Y);
+            angle += angleStep;
+            return point;
+        }
     }
 }
---
+++ cs/TagsCloudVisualization/Visualiser.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace TagsCloudVisualization
+{
+    public class Visualiser
+    {
+        public static void Main()
+        {
+            CircularCloudLayouter ccl = new CircularCloudLayouter(new Point(2500, 2500));
+            var rectSize = new Size(500, 500);
+            for (var i = 0; i < 50; i++)
+            {
+                ccl.PutNextRectangle(rectSize);
+            }
+            
+            var bitmap = new Bitmap(5000, 5000);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            SolidBrush brush = new SolidBrush(Color.Black);
+            Pen pen = new Pen(Color.Red);
+            graphics.Clear(Color.White);
+            graphics.FillRectangles(brush, ccl.field.ToArray());
+            graphics.DrawRectangles(pen, ccl.field.ToArray());
+            bitmap.Save("image.png", ImageFormat.Png);
+        }
+    }
+}
